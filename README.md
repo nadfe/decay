1069eb5a213a6ac3114d182a14f3ee84af25cf69
glossed <kyl3@riseup.net>
2020-03-29 00:22:02 -0400

    help

---
+++ README.md
@@ -0,0 +1,36 @@
+decay selfbot - by gloss
+==========
+
+
+Dependencies
+----------
+**All platforms**
+[Python 3.5.3 or higher](https://www.python.org/downloads/)
+[discord.py](https://pypi.org/project/colorama/)
+[Colorama](https://pypi.org/project/colorama/)
+[Requests](https://pypi.org/project/requests/)
+
+**Linux or MacOS**
+[lolcat](https://github.com/busyloop/lolcat)
+
+Installing
+----------
+
+  # Linux/macOS
+  python3 -m pip install -r requirements.txt
+
+  # Windows
+  py -3 -m pip install -r requirements.txt
+
+Setup and starting the self-bot
+----------
+
+Add your [discord auth token](https://github.com/Tyrrrz/DiscordChatExporter/wiki/Obtaining-Token-and-Channel-IDs#how-to-get-user-token) in token.json file and execute main.py.
+
+Command
+----------
+* "av" - Prints avatar url in the selected channel.
+* "cl" - Prunes all or a certain amount of messages in the entered channel or direct message channel.
+* "PING" - Prints your current Discord latency.
+* "PRICE" - Prints current Bitcoin and Ethereum exchange rate in the entered channel.
+* "rsc" - Restarts script from within itself.
---
+++ main.py
@@ -0,0 +1,85 @@
+import discord
+from discord.ext import commands
+import json
+import os
+from os import system, name
+import random
+import requests
+import subprocess
+import sys
+from colorama import Fore
+
+bot = commands.Bot(command_prefix='', self_bot=True, fetch_offline_members=False)
+bot.remove_command('help')
+
+# ASCII Splash Screens
+@bot.event
+async def on_ready():
+    def ascii():
+        i = random.randrange(11) + 1
+        if sys.platform == 'win32':
+            os.system("cls")
+            print(open("./art/" + str(i) + '.txt').read())
+            print()
+            print(Fore.WHITE + "[+] {0}".format(bot.user) + Fore.RED)
+        elif sys.platform == 'linux' or 'darwin':
+            os.system("clear")
+            os.system("cat art/" + str(i) + '.txt | lolcat')
+            print()
+            print(Fore.WHITE + "[+] {0}".format(bot.user) + Fore.RED)
+    ascii()
+
+# Retrieves bot user's Discord WebSocket latency.
+@bot.command()
+async def PING(ctx):
+    await ctx.message.edit(content=f":ping_pong:`Ping: {round(bot.latency * 1000)}ms`")
+
+# Restarts script from within itself without spawning a new terminal window.
+@bot.command()
+async def rsc(ctx):
+    await ctx.message.delete()
+    subprocess.signal.SIGINT
+    os.execl(sys.executable, sys.executable, * sys.argv)
+
+# Message clear command. Clearing with a set amount currently counts messages from all users and not the bot user.
+@bot.command()
+async def cl(ctx, amount=None):
+    if not amount:
+        async for ctx.message in ctx.channel.history(limit=13370):
+            if ctx.message.author == bot.user:
+                await ctx.message.delete()
+    else:
+        try:
+            async for ctx.message in ctx.channel.history(limit=int(amount) + 1):
+                if ctx.message.author == bot.user:
+                    await ctx.message.delete()
+        except discord.Forbidden:
+            pass
+
+# Crypto price command. Uses cryptowatch API.
+@bot.command()
+async def PRICE(ctx):
+    embed = discord.Embed(color=0xf2a900)
+    price = 'https://api.cryptowat.ch/markets/kraken/btcusd/price'
+    response = requests.get(price).json()
+    btc = str(response["result"]["price"])
+    price = 'https://api.cryptowat.ch/markets/kraken/ethusd/price'
+    response = requests.get(price).json()
+    eth = str(response["result"]["price"])
+    embed.add_field(name="BTC", value="$" + btc, inline=False)
+    embed.add_field(name="ETH", value="$" + eth, inline=False)
+    await ctx.send(embed=embed)
+
+# Retrieves avatar from any user from any mutual server. For more accurate results, its best to mention the user but just appending the username works fine.
+@bot.command()
+async def av(ctx, user: discord.User = None):
+    if not user:
+        avatar = ctx.author.avatar_url_as(static_format='png', size=1024)
+        await ctx.send(avatar)
+    else:
+        avatar = user.avatar_url_as(static_format='png', size=1024)
+        await ctx.send(avatar)
+
+# Accesses token from json file to prevent token leaks when sending a signal interrupt or editing code while screensharing with people :)
+token = json.loads(open("token.json").read())['token']
+bot.run(token, bot=False)
